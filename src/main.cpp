a7dc3f429dca2a93902cb136508bd392260928f5
Zesty <adi8116315284@gmail.com>
2025-01-20 23:54:36 +0530

    Added functionality to know the integer value of the character entered

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
 
-bool accept() {
-  std::cout << "Do you want to proceed (y or n) ?\n";
-
-  char answer = 0;
-  std::cin >> answer;
-
-  switch (answer) {
-    case 'y':
-      return true;
-    case 'n':
-      return false;
-    default:
-      std::cout << "I'll take that as a no.\n";
-      return false;
-  }
-}
-
 int main() {
-  bool answer = 0;
-
-  do {
-    answer = accept();
-  } while (answer);
-
+  char c;
+  std::cin >> c;
+  std::cout << "The value of " << c << " is " << int(c) << '\n';
   return 0;
 }
